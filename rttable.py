948aa7cdcd117645480419b49a09d9656df01fe9
Israel G. Lugo <israel.lugo@lugosys.com>
2016-11-27 22:30:46 +0000

    rttable: New TCAM class. Use it for efficient route lookups.

--- rttable.py
+++ rttable.py
@@ -27,6 +27,86 @@
 import netaddr
 
 
+class TCAM:
+    """Ternary Content-Addressable Memory.
+
+    Provides quick lookup access to existing routes. Can perform exact
+    lookups or longest match.
+
+    """
+
+    def __init__(self):
+        """Initialize an empty TCAM."""
+        self.dests_by_len = {}
+
+    def add(self, r):
+        """Add a route to the TCAM."""
+        destlen = r.destlen
+
+        routes_by_dest = self.dests_by_len.get(destlen)
+
+        if routes_by_dest is not None:
+            routes_by_dest[r.dest] = r
+        else:
+            self.dests_by_len[destlen] = {r.dest: r}
+
+    def get_exact(self, dest):
+        """Lookup in the TCAM for an exact match for dest.
+
+        dest must be a netaddr.IPNetwork object. Returns a Route object,
+        or None if no exact match exists.
+
+        """
+        if not isinstance(dest, netaddr.IPNetwork):
+            # we're hashing dest in dests_by_len; it needs to be the right
+            # type otherwise we won't match anything
+            raise TypeError("dest must be a netaddr.IPNetwork")
+
+        try:
+            routes_by_dest = self.dests_by_len[dest.prefixlen]
+
+            r = routes_by_dest[dest]
+        except KeyError:
+            r = None
+
+        return r
+
+    def longest_match(self, dest):
+        """Lookup in the TCAM for the longest match for dest.
+
+        dest should be either a netaddr.IPAddress or a netaddr.IPNetwork.
+        Returns a Route object, or None if no match exists.
+
+        """
+        if dest.version == 4:
+            bits = 32
+        elif dest.version == 6:
+            bits = 128
+        else:
+            raise ValueError("dest.version must be either 4 or 6")
+
+        # if dest has a prefixlen, make sure we only accept routes that can
+        # contain it in its entirety
+        maxlen = getattr(dest, 'prefixlen', bits)
+        assert maxlen <= bits
+
+        # get the IP from dest (if it's a network), or dest itself
+        ip = getattr(dest, 'ip', dest)
+        ip_str = str(ip)
+
+        interesting_lens = filter(lambda x: x <= maxlen, self.dests_by_len)
+
+        r = None
+        for destlen in sorted(interesting_lens, reverse=True):
+            truncated = netaddr.IPNetwork("{:s}/{:d}".format(ip_str, destlen),
+                                          flags=netaddr.NOHOST)
+            r = self.dests_by_len[destlen].get(truncated, None)
+            if r is not None:
+                break
+
+        return r
+
+
 class RoutingTable:
     def __init__(self, routes=None):
         """Initialize a RoutingTable.
@@ -35,36 +115,31 @@ class RoutingTable:
         path.
 
         """
+        self.tcam = TCAM()
         self.routes = []
-        self.routes_by_dest = {}
 
         if routes is not None:
             for r in routes:
-                r_ = self.routes_by_dest.get(r.dest)
+                r_ = self.tcam.get_exact(r.dest)
 
                 if r_ is None:
+                    self.tcam.add(r)
                     self.routes.append(r)
-                    self.routes_by_dest[r.dest] = r
                 else:
                     r_.add_nexthops(r.nexthops)
 
     def __iter__(self):
+        """Return an iterator over the routes."""
+
         return self.routes.__iter__()
 
-    def get_route_for(self, addr):
-        """Get longest matching route for an address.
+    def get_route_for(self, dest):
+        """Get longest matching route for an address or network.
 
-        Returns the most specific Route object that matches the specified
-        address, or None if none exists.
+        dest should be either a netaddr.IPAddress or a netaddr.IPNetwork.
+        Returns the most specific Route object that matches dest, or None
+        if none exists.
 
         """
-        # netaddr.smallest_matching_cidr sorts the routes and does a linear
-        # search; this isn't very efficient and probably shouldn't be used
-        # for a full Internet table. We should either 1) implement a trie, or
-        # 2) precalculate several dicts based on the prefixlens that occur
-        # in the table, then, for each prefixlen starting from the longest,
-        # lookup the address bitwise-and with the netmask for that
-        # prefixlen. Space-speed tradeoff.
-        prefix = netaddr.smallest_matching_cidr(addr, self.routes_by_dest)
+        return self.tcam.longest_match(dest)
 
-        return self.routes_by_dest[prefix] if prefix is not None else None
