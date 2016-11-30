a112479a609caba07722235a09fbd37c82333d55
Israel G. Lugo <israel.lugo@lugosys.com>
2016-11-30 17:14:55 +0000

    New example.conf file, using HOCON syntax. See #1.
    Will be used as a guide for what we want to do.
    https://github.com/typesafehub/config/blob/master/HOCON.md
    
    Skeleton for new module config.py. Will implement the configuration
    parser.

---
+++ config.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+# NetForeman - making sure your network is running smoothly
+# Copyright (C) 2016 Israel G. Lugo
+#
+# This file is part of NetForeman.
+#
+# NetForeman is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# NetForeman is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with NetForeman. If not, see <http://www.gnu.org/licenses/>.
+#
+# For suggestions, feedback or bug reports: israel.lugo@lugosys.com
+
+
+"""Configuration parser."""
+
+import pyhocon
+
+# TODO: Finish implementing this
+
+def print_syntax_error(e):
+    """Print location of a parse error in the source.
+    
+    Receives a pyparsing.ParseSyntaxException raised from the parse error.
+
+    """
+    print("{:s}\n{:>{width}}".format(e.line, "^", width=e.col))
