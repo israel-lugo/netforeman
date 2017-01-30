israel-lugo/netforeman

commit fdf13ab071aca7a270652208fb0e6583d2586891

Module init no longer executes behavior. New ModuleInfo class.
The goal is to separate module initialization from module execution.

netforeman/config.py
@@ -36,6 +36,15 @@ class ParseError(Exception):
         return self.message
 
 
+class ModuleInfo:
+    """Information about a loaded module."""
+
+    def __init__(self, name, module, api):
+        self.name = name
+        self.module = module
+        self.api = api
+
+
 class Configurator:
 
     def __init__(self, filename):
@@ -58,17 +67,18 @@ class Configurator:
         except pyhocon.exceptions.ConfigMissingException as e:
             raise ParseError("missing mandatory section 'modules'")
 
-        # load the modules
-        self.modules = [
-                importlib.import_module("{:s}.{:s}".format(__package__, module))
-            for module in modules_to_load
-        ]
+        self.loaded_modules = []
+        self.modules_by_name = {}
+        for name in modules_to_load:
+            if name in self.modules_by_name:
+                raise ParseError("module '{:s}' duplicated, already loaded".format(name))
 
-        # create the API object for every loaded module
-        self.module_apis = [
-            m.API(self.get_module_conf(m))
-            for m in self.modules
-        ]
+            module = importlib.import_module("{:s}.{:s}".format(__package__, name))
+            api = module.API(self.get_module_conf(module))
+            modinfo = ModuleInfo(name, module, api)
+
+            self.loaded_modules.append(modinfo)
+            self.modules_by_name[name] = modinfo
 
     def get_module_conf(self, module):
         """Get the a module's config tree.

netforeman/fibinterface.py
@@ -120,7 +120,12 @@ class FIBModuleAPI(netforeman.moduleapi.ModuleAPI):
         # means this should be an abstract class.
         self.fib = self._create_fib()
 
-        for subconf in conf.get_list('route_checks', default=[]):
+        self.conf = conf
+
+    def run(self):
+        """Run any configured verifications and actions in this module."""
+
+        for subconf in self.conf.get_list('route_checks', default=[]):
             self.route_check(subconf)
 
     def route_check(self, conf):

netforeman/moduleapi.py
@@ -36,6 +36,10 @@ class ModuleAPI(metaclass=abc.ABCMeta):
         Receives a pyhocon.config_tree.ConfigTree object, containing the
         module's config tree.
 
+        This method is only for initialization and should not execute any
+        independent behavior, such as user-configured checks and actions.
+        The run method should be used for that.
+
         """
         pass
 
@@ -44,6 +48,19 @@ class ModuleAPI(metaclass=abc.ABCMeta):
     def actions(self):
         """Get the module's actions."""
         return {}
+
+    # This method need not be overriden if the module doesn't do anything
+    # by itself (e.g. it only exists to provide callable actions).
+    def run(self):
+        """Run any configured verifications and actions in this module.
+
+        This method should be overriden by modules with their own
+        independent behavior, e.g. modules that perform user-configured
+        verifications or actions by themselves. Modules which only provide
+        callable actions (e.g. sendemail) need not override this.
+
+        """
+        pass
 
     @staticmethod
     def _get_conf(conf, name, required=True):

commit 1fd786e0a62d2c7003f6ef7e7be1ed96c7cdc704

Fix duplicate module prevention. Broken since fdf13ab0.
We changed the exception to a warning, and forgot to skip the loop
iteration.

netforeman/config.py
@@ -157,6 +157,7 @@ class Configurator:
 
             if name in self.modules_by_name:
                 self.logger.warning("ignoring duplicate entry for module '%s', already loaded", name)
+                continue
 
             try:
                 modinfo = self.load_module(name)