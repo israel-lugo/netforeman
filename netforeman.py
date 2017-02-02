31bdd08feb6e5247ac96a25d39a17203d6d01b63
Israel G. Lugo <israel.lugo@lugosys.com>
2017-02-02 12:55:47 +0000

    New script to run from source without installing.

---
+++ netforeman.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+# NetForeman - making sure your network is running smoothly
+# Copyright (C) 2016, 2017 Israel G. Lugo
+#
+# This file is part of NetForeman.
+#
+# NetForeman is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# NetForeman is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with NetForeman. If not, see <http://www.gnu.org/licenses/>.
+#
+# For suggestions, feedback or bug reports: israel.lugo@lugosys.com
+
+
+"""Making sure your network is running smoothly.
+
+This is the command line script, for running from the source directory
+without installing.
+
+"""
+
+import sys
+from netforeman.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
+# vim: set expandtab smarttab shiftwidth=4 softtabstop=4 tw=75 :
